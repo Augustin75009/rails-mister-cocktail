de5440eaee7913d1f7380327404d831764bb5972
augustin cousin <augustincousin.pro@gmail.com>
2018-11-22 18:35:09 +0100

    exo2

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'doses/new'
+  get 'doses/index'
+  delete "cocktails/:id", to: "cocktails#destroy", as: :destroy
+  delete "doses/:id", to: "doses#destroy", as: :dose_destroy
+  resources :cocktails do
+    resources :doses
+  end
+  resources :doses, only: [:edit, :new, :destroy]
+
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
